40e44338a96db56ef3dc39549ef72aab7a94d274
MatiasKala <matiraffi@gmail.com>
2021-06-10 19:38:07 -0300

    Componentes creados y dependencias instaladas

---
+++ parcial10-6/src/components/MultipleChoice.vue
@@ -0,0 +1,37 @@
+<template>
+
+  <section class="multiple-choice">
+    <h1>multiple-choice Component</h1>
+  </section>
+
+</template>
+
+<script>
+
+  export default  {
+    name: 'multiple-choice',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped>
+  .multiple-choice {
+
+  }
+</style>
